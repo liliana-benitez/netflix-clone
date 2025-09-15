96d861e2345ab23c84efc188770d8ba8790e9bab
Liliana Benitez <lilyybenitezz@gmail.com>
2025-09-15 17:46:02 +0200

    organize project files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
 function App() {
-  return <>Netflix</>
+  return (
+    <>
+      <h1>Netflix</h1>
+
+      <p>Hero Section</p>
+
+      <p>Watchlist</p>
+
+      <p>Movies</p>
+      <p>Movies List</p>
+
+      <p>TV Shows</p>
+      <p>TV Shows List</p>
+    </>
+  )
 }
 
 export default App
