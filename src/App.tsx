f6f2b3047731cd37b7b315ea4f00521003921627
Liliana Benitez <lilyybenitezz@gmail.com>
2025-09-15 17:42:02 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <>Netflix</>
+}
+
+export default App
---
+++ src/utils/tmdb-api.ts
@@ -0,0 +1,36 @@
+const API_TOKEN = import.meta.env.VITE_TMDB_API_KEY
+const BASE_URL = "https://api.themoviedb.org/3"
+
+async function fetchData(url: string) {
+  return await fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      return data
+    })
+}
+
+const MoviesAPI = {
+  async getMovies() {
+    const data = await fetchData(
+      `${BASE_URL}/trending/movie/day?api_key=${API_TOKEN}`
+    )
+    return data.results
+  },
+
+  async getTVShows() {
+    const data = await fetchData(
+      `${BASE_URL}/trending/tv/day?api_key=${API_TOKEN}`
+    )
+    return data.results
+  },
+
+  async getMovie(id: number) {
+    return fetchData(`${BASE_URL}/trending/movie/${id}?api_key=${API_TOKEN}`)
+  },
+
+  async getTVShow(id: number) {
+    return fetchData(`${BASE_URL}/trending/tv/${id}?api_key=${API_TOKEN}`)
+  }
+}
+
+export default MoviesAPI
